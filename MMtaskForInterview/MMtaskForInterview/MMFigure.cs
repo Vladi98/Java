0b72878697ad34e47d1610839bf7d9b0137d3db3
Vladimir Dachkinov <37123303+Vladi98@users.noreply.github.com>
2019-05-08 13:39:53 +0300

    Add files via upload

---
+++ MMtaskForInterview/MMtaskForInterview/MMFigure.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MMtaskForInterview
+{
+    public class MMFigure
+    {
+        private int size;
+        private StringBuilder sb;
+
+        public MMFigure(int size)
+        {
+            this.size = size;
+            this.sb = new StringBuilder();
+        }
+
+        public string BuildUpperPart()
+        {
+            this.sb.Clear();
+            int a = 0;
+            int b = this.size;
+
+            for (int i = 0; i < this.size / 2 + 1; i++)
+            {
+
+                this.sb.Append(new string('-', this.size - i));
+                this.sb.Append(new string('*', b));
+                this.sb.Append(new string('-', this.size - a));
+                this.sb.Append(new string('*', b));
+                this.sb.Append(new string('-', this.size * 2 - a));
+
+                this.sb.Append(new string('*', b));
+                this.sb.Append(new string('-', this.size - a));
+                this.sb.Append(new string('*', b));
+                this.sb.Append(new string('-', this.size - i));
+
+                this.sb.AppendLine();
+                b += 2;
+                a += 2;
+
+            }
+            return this.sb.ToString().Trim();
+        }
+
+        public string BuildLowerPart()
+        {
+            this.sb.Clear();
+            int a = 1;
+
+            for (int i = 0; i < this.size / 2 + 1; i++)
+            {
+                this.sb.Append(new string('-', this.size / 2 - i));
+                this.sb.Append(new string('*', this.size));
+                this.sb.Append(new string('-', a));
+                this.sb.Append(new string('*', this.size * 2 - a));
+
+                this.sb.Append(new string('-', a));
+                this.sb.Append(new string('*', this.size));
+                this.sb.Append(new string('-', this.size - a));
+
+                this.sb.Append(new string('*', this.size));
+                this.sb.Append(new string('-', a));
+                this.sb.Append(new string('*', this.size * 2 - a));
+
+                this.sb.Append(new string('-', a));
+                this.sb.Append(new string('*', this.size));
+                this.sb.Append(new string('-', this.size / 2 - i));
+
+
+                this.sb.AppendLine();
+                a += 2;
+            }
+            return this.sb.ToString().Trim();
+        }
+
+    }
+}
---
+++ MMtaskForInterview/MMtaskForInterview/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MMtaskForInterview
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            MMFigure mm = new MMFigure(n);
+
+            Console.WriteLine(mm.BuildUpperPart());
+            Console.WriteLine(mm.BuildLowerPart());
+        }
+    }
+}
